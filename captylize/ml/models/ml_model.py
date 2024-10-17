7e76d626abb039743b8d9e582f5834b66b54315e
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 21:29:52 +0200

    feat: add device input param

--- captylize/ml/models/ml_model.py
+++ captylize/ml/models/ml_model.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Literal
 from PIL import Image
 
 
 class Img2TextModel(ABC):
-    def __init__(self, cache_dir: str):
+    def __init__(self, cache_dir: str, device: Literal["cpu", "cuda", "mps"]):
         self.cache_dir = cache_dir
+        self.device = device
 
     @abstractmethod
     def load(self):
