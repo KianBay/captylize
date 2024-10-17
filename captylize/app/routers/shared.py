eeb0d56f99385ab50fcca153d8561fe7f698065e
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 20:42:24 +0200

    fix: add correct errors in routers.shared

--- captylize/app/routers/shared.py
+++ captylize/app/routers/shared.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, HTTPException, Form, File, UploadFile
+from fastapi import Form, File, UploadFile
+from pydantic import ValidationError
 
 from captylize.app.dtos.shared import ImageRequest, ALLOWED_IMAGE_EXTENSIONS
 
@@ -7,23 +8,18 @@ async def validate_image_input(
     image_url: str = Form(default=None), image_file: UploadFile = File(default=None)
 ) -> ImageRequest:
     if image_file and image_file.filename and image_url:
-        raise HTTPException(
-            status_code=400, detail="Provide either image_url or image_file, not both."
-        )
+        raise ValueError("Provide either image_url or image_file, not both.")
 
     if image_file and image_file.filename:
         if not any(
             image_file.filename.lower().endswith(ext)
             for ext in ALLOWED_IMAGE_EXTENSIONS
         ):
-            raise HTTPException(
-                status_code=400,
-                detail=f"Invalid image file type. Must be one of {ALLOWED_IMAGE_EXTENSIONS}",
+            raise ValidationError(
+                f"Invalid image file type. Must be one of {ALLOWED_IMAGE_EXTENSIONS}"
             )
         return ImageRequest(image_file=image_file)
     elif image_url:
         return ImageRequest(image_url=image_url)
     else:
-        raise HTTPException(
-            status_code=400, detail="Either image_url or image_file must be provided."
-        )
+        raise ValueError("Either image_url or image_file must be provided.")
