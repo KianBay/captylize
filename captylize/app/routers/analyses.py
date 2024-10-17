3b9000472eefa62fa38c60a75b7e75eae8d3967d
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 18:43:09 +0200

    feat: add initial routes

---
+++ captylize/app/routers/analyses.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from app.dtos.analyses.request import AgeRequest, MoodRequest, NSFWRequest
+
+router = APIRouter(prefix="/analyses", tags=["analyses"])
+
+
+@router.post("/ages")
+async def create_age_analysis(analysis: AgeRequest):
+    return analysis
+
+
+@router.post("/moods")
+async def create_mood_analysis(analysis: MoodRequest):
+    return analysis
+
+
+@router.post("/nsfw")
+async def create_nsfw_analysis(analysis: NSFWRequest):
+    return analysis
