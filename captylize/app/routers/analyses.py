41d7e6f1ae8cf7e84d83e966dba7e66f0ec0700c
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 20:38:12 +0200

    feat: add image opening, error handling

--- captylize/app/routers/analyses.py
+++ captylize/app/routers/analyses.py
@@ -1,28 +1,44 @@
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import ValidationError
 from captylize.app.dtos.analyses.request import AgeRequest, MoodRequest, NSFWRequest
 from captylize.app.routers.shared import validate_image_input
+from captylize.app.utils import get_image
 
 router = APIRouter(prefix="/analyses", tags=["analyses"])
 
 
 @router.post("/ages")
 async def create_age_analysis(image_input: AgeRequest = Depends(validate_image_input)):
-    if image_input.image_url:
-        print("Analyzing from url!")
-        result = {"type": "URL", "url": str(image_input.image_url)}
-    elif image_input.image_file:
-        print("Analyzing from file!")
-        result = {"type": "FILE", "filename": image_input.image_file.filename}
-    else:
-        raise HTTPException(status_code=400, detail="No image provided")
-    return result
+    image = await get_image(image_input.image_url, image_input.image_file)
+    try:
+        return {"width": image.width, "height": image.height}
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/moods")
-async def create_mood_analysis(analysis: MoodRequest):
-    return analysis
+async def create_mood_analysis(
+    image_input: MoodRequest = Depends(validate_image_input),
+):
+    try:
+        image = await get_image(image_input.image_url, image_input.image_file)
+        return {"width": image.width, "height": image.height}
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/nsfw")
-async def create_nsfw_analysis(analysis: NSFWRequest):
-    return analysis
+async def create_nsfw_analysis(
+    image_input: NSFWRequest = Depends(validate_image_input),
+):
+    try:
+        image = await get_image(image_input.image_url, image_input.image_file)
+        return {"width": image.width, "height": image.height}
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
