5b11a712ffc1abb1242d63e753b359b4e75603a0
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 22:21:33 +0200

    fix: allow lifespan to be called by the app

--- captylize/app/main.py
+++ captylize/app/main.py
@@ -2,27 +2,33 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 
+from captylize.logger import get_logger
 from captylize.app.routers import analyses
 from captylize.app.http_client import async_session
 from captylize.ml.manager import model_manager
 
-app = FastAPI(root_path="/api/v1")
+logger = get_logger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting up")
+    # Before yield is on startup
+    model_manager.load_models()
+    yield
+    # After yield is on shutdown
+    model_manager.unload_models()
+    logger.info("Shutting down")
+    await async_session.close()
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 app.include_router(analyses.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Before yield is on startup
-    model_manager.load_models()
-    yield
-    # After yield is on shutdown
-    model_manager.unload_models()
-    await async_session.close()
 
 
 if __name__ == "__main__":
