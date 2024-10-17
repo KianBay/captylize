c724e5d4f63cb915976b70010e9460ba5bd71106
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-17 22:05:32 +0200

    feat: dependency function to get age model

---
+++ captylize/app/dependencies/ml_models.py
@@ -0,0 +1,9 @@
+from fastapi import Depends, Query
+from captylize.ml.manager import model_manager
+from captylize.ml.models.ml_model import Img2TextModel
+
+
+async def get_age_model(
+    model_name: str = Query(None, description="The name of the model to use"),
+) -> Img2TextModel:
+    return model_manager.get_age_model(model_name)
