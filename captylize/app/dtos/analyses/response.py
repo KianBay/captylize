311e650c75df4656d82e7891e39b613037407753
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-19 02:37:31 +0200

    revert: change back to dict based prob on models

--- captylize/app/dtos/analyses/response.py
+++ captylize/app/dtos/analyses/response.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from captylize.app.dtos.shared import InferenceResponse
 
@@ -8,22 +8,7 @@ AgeRange = Literal[
 ]
 
 
-class AgeRangeProb(BaseModel):
-    age_range: AgeRange
-    probability: float
-
-
 Emotion = Literal["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
-
-
-class EmotionProb(BaseModel):
-    emotion: Emotion
-    probability: float
-
-
-class AgeResponse(InferenceResponse):
-    predicted_age: AgeRange
-    probabilities: list[AgeRangeProb]
 
 
 # Nsfw categories are a todo for now,
@@ -33,12 +18,14 @@ class AgeResponse(InferenceResponse):
 NsfwCategory = Literal["sfw", "nsfw", "normal"]
 
 
-class NsfwProb(BaseModel):
-    category: NsfwCategory
-    probability: float
+class AgeResponse(InferenceResponse):
+    predicted_age: AgeRange
+    probabilities: dict[AgeRange, float]
 
     @classmethod
-    def from_prediction(cls, prediction: dict[str, float], prediction_duration: float):
+    def from_prediction(
+        cls, prediction: dict[AgeRange, float], prediction_duration: float
+    ):
         predicted_age = max(prediction, key=prediction.get)
         return cls(
             predicted_age=predicted_age,
@@ -49,10 +36,12 @@ class NsfwProb(BaseModel):
 
 class EmotionResponse(InferenceResponse):
     predicted_emotion: Emotion
-    probabilities: list[EmotionProb]
+    probabilities: dict[Emotion, float]
 
     @classmethod
-    def from_prediction(cls, prediction: dict[str, float], prediction_duration: float):
+    def from_prediction(
+        cls, prediction: dict[Emotion, float], prediction_duration: float
+    ):
         predicted_emotion = max(prediction, key=prediction.get)
         return cls(
             predicted_emotion=predicted_emotion,
@@ -63,10 +52,12 @@ class EmotionResponse(InferenceResponse):
 
 class NSFWResponse(InferenceResponse):
     predicted_category: NsfwCategory
-    probabilities: list[NsfwProb]
+    probabilities: dict[NsfwCategory, float]
 
     @classmethod
-    def from_prediction(cls, prediction: dict[str, float], prediction_duration: float):
+    def from_prediction(
+        cls, prediction: dict[NsfwCategory, float], prediction_duration: float
+    ):
         predicted_category = max(prediction, key=prediction.get)
         return cls(
             predicted_category=predicted_category,
