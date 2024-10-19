ce555d0bad8e7399b68760c9edaac91789777148
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-19 02:46:50 +0200

    feat: add field docs to enhance the openapi experience for response models in analyses

--- captylize/app/dtos/analyses/response.py
+++ captylize/app/dtos/analyses/response.py
@@ -1,26 +1,50 @@
-from typing import Literal
-from pydantic import BaseModel
+from enum import StrEnum
+
+from pydantic import Field
 
 from captylize.app.dtos.shared import InferenceResponse
 
-AgeRange = Literal[
-    "0-2", "3-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"
-]
+
+class AgeRange(StrEnum):
+    AGE_0_2 = "0-2"
+    AGE_3_9 = "3-9"
+    AGE_10_19 = "10-19"
+    AGE_20_29 = "20-29"
+    AGE_30_39 = "30-39"
+    AGE_40_49 = "40-49"
+    AGE_50_59 = "50-59"
+    AGE_60_69 = "60-69"
+    AGE_70_PLUS = "70+"
 
 
-Emotion = Literal["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+class Emotion(StrEnum):
+    ANGRY = "angry"
+    DISGUST = "disgust"
+    FEAR = "fear"
+    HAPPY = "happy"
+    SAD = "sad"
+    SURPRISE = "surprise"
+    NEUTRAL = "neutral"
 
 
 # Nsfw categories are a todo for now,
 # Some models classify as sfw/nsfw, others as normal/nsfw, others as normal/porn/sexy etc
 # For now we stick to easily castable of FalconsAI & AdamCobb versions, which are normal/nsfw and sfw/nsfw respectively
-
-NsfwCategory = Literal["sfw", "nsfw", "normal"]
+class NsfwCategory(StrEnum):
+    SFW = "sfw"
+    NSFW = "nsfw"
 
 
 class AgeResponse(InferenceResponse):
-    predicted_age: AgeRange
-    probabilities: dict[AgeRange, float]
+    predicted_age: AgeRange = Field(
+        ...,
+        description="The predicted age range, i.e. age range with highest probability.",
+    )
+    probabilities: dict[AgeRange, float] = Field(
+        ...,
+        description="The probabilities of each age range",
+        example={age_range: 0.0 for age_range in AgeRange},
+    )
 
     @classmethod
     def from_prediction(
@@ -35,8 +59,15 @@ class AgeResponse(InferenceResponse):
 
 
 class EmotionResponse(InferenceResponse):
-    predicted_emotion: Emotion
-    probabilities: dict[Emotion, float]
+    predicted_emotion: Emotion = Field(
+        ...,
+        description="The predicted emotion, i.e. emotion with highest probability.",
+    )
+    probabilities: dict[Emotion, float] = Field(
+        ...,
+        description="The probabilities of each emotion",
+        example={emotion: 0.0 for emotion in Emotion},
+    )
 
     @classmethod
     def from_prediction(
@@ -51,8 +82,15 @@ class EmotionResponse(InferenceResponse):
 
 
 class NSFWResponse(InferenceResponse):
-    predicted_category: NsfwCategory
-    probabilities: dict[NsfwCategory, float]
+    predicted_category: NsfwCategory = Field(
+        ...,
+        description="The predicted NSFW category - exact fields may vary by model; check documentation for specific model. Always expect sfw and nsfw.",
+    )
+    probabilities: dict[NsfwCategory, float] = Field(
+        ...,
+        description="The probabilities of each NSFW category - exact fields may vary by model; check documentation for specific model. Always expect sfw and nsfw.",
+        example={category: 0.0 for category in NsfwCategory},
+    )
 
     @classmethod
     def from_prediction(
